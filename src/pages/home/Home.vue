3e719df9120effd32c7b23f78f1f785f6f142542
Thyago Hoffman <thoffman1698@gmail.com>
2019-08-15 10:51:26 -0300

    Teste

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="content">
+    <img src="../../assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'content',
+    data () {
+      return{
+        msg: "Título Teste"
+      }
+    }
+  }
+</script>
+
+<style>
+  #content {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
